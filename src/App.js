2a028a8f5a2625b73dff572173dfee2dff3a148c
Paul Newby <paulnewby@Pauls-iMac.home>
2020-07-29 14:40:00 +0100

    Created a rudimentary drum machine with React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sounds = [
+  {
+    key: 'Q',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    key: 'W',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+
+  },
+  {
+    key: 'E',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' 
+  },
+  {
+    key: 'R',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    key: 'T',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    key: 'Y',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    key: 'U',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    key: 'I',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    key: 'O',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  },
+]
+
+
+const App = () => (
+    <div id="drum-machine" className="container">
+      <div id="display" className="display">
+        <h1>Press a key</h1>
+        {sounds.map((sound, idx) => (
+        <Box text={sound.key} key={idx} audio={sound.mp3}/>
+        ))}
+        </div>
+      </div>
+    );
+
+
+class Box extends React.Component  {
+  constructor(props) {
+    super(props);
+
+    this.audio = React.createRef();
+  }
+  componentDidMount() {
+    this.audio.current.addEventListener('ended', (e) => {
+      const parent = e.target.parentNode;
+       parent.classList.remove('active');
+  });
+  }
+
+  playSound = () => {
+    this.audio.current.play();
+
+    const id = this.audio.current.id;
+
+    const parent = this.audio.current.parentNode;
+    parent.classList.add('active');
+
+    const display = parent.parentNode;
+    display.querySelector('h1').innerText = `${id}`;
+  }
+
+
+    render() {
+      const { text, audio} = this.props;
+
+      return (
+        <div className="box" onClick={this.playSound} onKeyDown={this.handleKey}>
+          {text}
+          <audio ref={this.audio} src={audio} className="clip" id={text} />
+        </div>
+      )
+   }
 }
+
+document.addEventListener('keydown', (e) => {
+  const id = e.key.toUpperCase();
+  const audio = document.getElementById(id);
+
+  if(audio) {
+    audio.currentTime = 0;
+    const parent = audio.parentNode;
+    parent.classList.add('active');
+    audio.play();
+  }
+ });
 
 export default App;
