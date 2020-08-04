cf28121da487e6e63f526bde3a726ded295586d3
Paul Newby <paulnewby@Pauls-iMac.home>
2020-08-04 19:26:08 +0100

    Updates

---
+++ precache-manifest.525bf70209485ff02f39e8598e718698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502a972ea702ca8051eb38746335d4d3",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "b37ef0b50d884bd74e98",
+    "url": "/drum-machine/static/css/main.25b2d83d.chunk.css"
+  },
+  {
+    "revision": "d6f0319ae71494d37fdb",
+    "url": "/drum-machine/static/js/2.3c1b4878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.3c1b4878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37ef0b50d884bd74e98",
+    "url": "/drum-machine/static/js/main.48fec9d6.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
